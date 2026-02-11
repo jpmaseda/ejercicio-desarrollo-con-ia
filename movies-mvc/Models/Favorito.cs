f98123026afc1ac466050515c4b65a021b288666
Juan Pablo <jpmaseda@gmail.com>
2026-02-11 20:05:07 -0300

    Id para Models.Favorito

--- movies-mvc/Models/Favorito.cs
+++ movies-mvc/Models/Favorito.cs
@@ -2,6 +2,10 @@
 {
     public class Favorito
     {
+        //Puedo usar Fluent API para la clase intermedia, pero como le agregue la Fecha a la clase Favorito,
+        //no puedo usarla como tabla intermedia, por lo que tengo que agregarle un Id a la clase Favorito
+        //para que sea una tabla normal y no una tabla intermedia 
+        public int Id { get; set; }
         public string UsuarioId { get; set; }
         public Usuario? Usuario { get; set; }
         public int PeliculaId { get; set; }
